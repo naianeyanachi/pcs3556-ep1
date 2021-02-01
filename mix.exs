0bb92bd23269b7ed7434b138ace80c97d2981d9b
naianeyanachi <naiane.yanachi@gmail.com>
2021-01-31 21:22:34 -0300

    ep1

---
+++ mix.exs
@@ -0,0 +1,28 @@
+defmodule EP1.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :ep1,
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:libgraph, "0.13.3"},
+      {:matrix, "0.3.2"}
+    ]
+  end
+end
